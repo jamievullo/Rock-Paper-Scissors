aab9dd41afec874ea4321fdae4c7c8667cfbd527
Jamie vullo <jvullo@protonmail.com>
2020-07-07 11:56:06 -0400

    Added buttons for R,P,S.

---
+++ app.js
@@ -0,0 +1,7 @@
+let Rock = document.getElementById('rock').addEventListener('click', runGame);
+let Paper = document.getElementById('paper').addEventListener('click', runGame);
+let Scissors = document.getElementById('scissors').addEventListener('click', runGame);
+
+function runGame() {
+
+}
